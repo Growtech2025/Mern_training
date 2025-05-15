7d431b429c6ac8210af762470c25b8aa971ad38e
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2025-05-15 16:30:59 +0530

    first class of js

---
+++ Frontend/js/class-1/script.js
@@ -0,0 +1,16 @@
+//For Printinng
+console.log("Hello Groww");
+console.log(10);
+console.log(true);
+console.log(undefined);
+console.log(null);
+
+var num = prompt();
+console.log(num);
+
+console.log(a);
+let a = 10;
+
+
+console.log(c)
+var c = 20;
