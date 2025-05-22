44295c238071a5bba1b72de0cde22cff3987b6c0
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2025-05-22 15:25:51 +0530

    class-6

---
+++ Frontend/js/class-6/script.js
@@ -0,0 +1,47 @@
+console.log("Hey i am js");
+
+
+const product = {
+    id: 1001,
+    name: "Smartphone",
+    isAvailable: true,
+    details: {
+        manufacturer: "TechCorp",
+        specs: {
+            processor: "Octa-core 2.2GHz",
+            memory: {
+                ram: "8GB",
+                colors: ["black", "silver", "blue"],
+                "data-info": {
+                    idf: 780,
+                    user: {
+                        name: "Lakhan",
+                        kaam: "Nachna ",
+                        colors: ["black", 24, 8.908, undefined],
+                        123: function() {
+                            console.log("hey karan namo namo ");
+                        }
+                    },
+                    greet: function() {
+                        console.log("Hey i am bot");
+                    }
+                },
+                storage: "128GB",
+                dimensions: {
+                    getSum: function(a, b) {
+                        return a + b;
+                    },
+                    width: "70mm"
+                },
+                height: "150mm",
+            },
+
+        },
+        colors: ["black", "silver", "blue"]
+    },
+    tags: ["electronics", "mobile", "gadget"]
+};
+
+
+
+console.log(product.details.specs.memory["data-info"].user["123"]())
