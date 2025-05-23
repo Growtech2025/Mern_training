8263ff6cebd96b361c04b54a5f0efd8024211613
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2025-05-23 17:34:05 +0530

    class-7

--- Frontend/js/class-6/script.js
+++ Frontend/js/class-6/script.js
@@ -1,50 +1,50 @@
-// console.log("Hey i am js");
+console.log("Hey i am js");
 
 
-// const product = {
-//     id: 1001,
-//     name: "Smartphone",
-//     isAvailable: true,
-//     details: {
-//         manufacturer: "TechCorp",
-//         specs: {
-//             processor: "Octa-core 2.2GHz",
-//             memory: {
-//                 ram: "8GB",
-//                 colors: ["black", "silver", "blue"],
-//                 "data-info": {
-//                     idf: 780,
-//                     user: {
-//                         name: "Lakhan",
-//                         kaam: "Nachna ",
-//                         colors: ["black", 24, 8.908, undefined],
-//                         123: function() {
-//                             console.log("hey karan namo namo ");
-//                         }
-//                     },
-//                     greet: function() {
-//                         console.log("Hey i am bot");
-//                     }
-//                 },
-//                 storage: "128GB",
-//                 dimensions: {
-//                     getSum: function(a, b) {
-//                         return a + b;
-//                     },
-//                     width: "70mm"
-//                 },
-//                 height: "150mm",
-//             },
+const product = {
+    id: 1001,
+    name: "Smartphone",
+    isAvailable: true,
+    details: {
+        manufacturer: "TechCorp",
+        specs: {
+            processor: "Octa-core 2.2GHz",
+            memory: {
+                ram: "8GB",
+                colors: ["black", "silver", "blue"],
+                "data-info": {
+                    idf: 780,
+                    user: {
+                        name: "Lakhan",
+                        kaam: "Nachna ",
+                        colors: ["black", 24, 8.908, undefined],
+                        123: function() {
+                            console.log("hey karan namo namo ");
+                        }
+                    },
+                    greet: function() {
+                        console.log("Hey i am bot");
+                    }
+                },
+                storage: "128GB",
+                dimensions: {
+                    getSum: function(a, b) {
+                        return a + b;
+                    },
+                    width: "70mm"
+                },
+                height: "150mm",
+            },
 
-//         },
-//         colors: ["black", "silver", "blue"]
-//     },
-//     tags: ["electronics", "mobile", "gadget"]
-// };
+        },
+        colors: ["black", "silver", "blue"]
+    },
+    tags: ["electronics", "mobile", "gadget"]
+};
 
 
 
-// console.log(product.details.specs.memory["data-info"].user["123"]());
+console.log(product.details.specs.memory["data-info"].user["123"]());
 
 
 
@@ -52,27 +52,27 @@
 
 
 
-// let arr = ["Mohan", { id: 101, kam: "Hashna" }, 500, [9.6, 9.7, 9.8]];
+let arr = ["Mohan", { id: 101, kam: "Hashna" }, 500, [9.6, 9.7, 9.8]];
 
-// console.log(arr[1].kam);
-
-
-
-// let tanishq = ["name", 3.5, "viashnav Dham", {
-//     place: "jammu",
-//     date: "26 / 05 / 2025",
-//     family: ["father", "mother", [50000, "prasad", {
-
-//         wapsaayga: true,
-//         ladaiakrega: "nhi",
-//         chalo: {
-//             temple: "Mahakal"
-//         }
-//     }]]
-// }]
+console.log(arr[1].kam);
 
 
-// console.log(tanishq[3].family[2][2].ladaiakrega);
+
+let tanishq = ["name", 3.5, "viashnav Dham", {
+    place: "jammu",
+    date: "26 / 05 / 2025",
+    family: ["father", "mother", [50000, "prasad", {
+
+        wapsaayga: true,
+        ladaiakrega: "nhi",
+        chalo: {
+            temple: "Mahakal"
+        }
+    }]]
+}]
+
+
+console.log(tanishq[3].family[2][2].ladaiakrega);
 
 
 
