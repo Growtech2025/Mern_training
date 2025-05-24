a97df02b2c561bed54b65273ace3b8a87022c922
Aviraj0714 <131906596+Aviraj0714@users.noreply.github.com>
2025-05-24 15:35:38 +0530

    class-8

--- Frontend/js/class-7/script.js
+++ Frontend/js/class-7/script.js
@@ -283,3 +283,21 @@
 
 // let nam = "krishna"
 // console.log('Count of vovles', countVovle(nam))
+
+
+
+//palindrome number
+let num = parseInt(prompt());
+
+function isPalindrome(num) {
+    let original = num;
+    let reversed = 0;
+
+    while (num > 0) {
+        let digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+
+    return original === reversed;
+}
---
+++ Frontend/js/class-8/script.js
@@ -0,0 +1,42 @@
+// function sayHi() {
+//     console.log("hi");
+// }
+// sayHi();
+
+
+
+// function companyDetails(cName) {
+//     let suffix = " own by DRF";
+//     return cName + suffix;
+// }
+
+
+// let op = companyDetails("GrowTech ");
+// console.log(op);
+// console.log(companyDetails("TechMahindra"));
+
+
+// const callBackAnnonymous = () => {
+//     console.log("This is a callback function");
+// }
+
+// callBackAnnonymous();
+
+
+
+// const naamWala = function NaamWala() => {
+//     console.log("naamWalaFunction")
+// }
+
+
+// var getData = () => console.log("This is a getData function");
+
+// getData();
+
+
+// try {
+//     console.log("how are you");
+//     throw new Error("Oops!"); // Uncomment to test
+// } catch (error) {
+//     console.log("Error caught:", error.message);
+// }
