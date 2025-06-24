a2e8d124e9d4dec5c2a361ef6742d6d22d01bcc5
Growtech2025 <aviraj7503@gmail.com>
2025-06-24 12:00:21 +0530

    React Form

---
+++ class9c1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import MyForm from './components/MyForm.jsx';
+import BestForm from './components/BestForm.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MyForm /> */}
+      <BestForm/>
+    </div>
+  );
+}
+
+export default App;
