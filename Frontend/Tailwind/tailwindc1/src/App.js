49fd49dfa8ac981294853dc0dd650ddd27859f49
Growtech2025 <aviraj7503@gmail.com>
2025-06-24 16:51:28 +0530

    Tailwind

---
+++ Frontend/Tailwind/tailwindc1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="bg-orange-600">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
